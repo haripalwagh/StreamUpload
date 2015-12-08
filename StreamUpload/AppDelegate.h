9cc43d77685607500493a5cce4827ae7325e67b5
haripalwagh <haripalwagh@gmail.com>
2015-12-08 11:44:18 +0530

    Initial Commit

---
+++ StreamUpload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StreamUpload
+//
+//  Created by Haripal on 12/8/15.
+//  Copyright © 2015 Haripal Wagh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StreamUpload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StreamUpload
+//
+//  Created by Haripal on 12/8/15.
+//  Copyright © 2015 Haripal Wagh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
